0aa5252cd6f5aaa9ba17c78d72de44c529ae244f
Daniele Megna <megna.dany@gmail.com>
2023-07-25 18:39:33 +0200

    Test damage calculation with readme provided example

--- java/src/test/java/codingdojo/PlayerTest.java
+++ java/src/test/java/codingdojo/PlayerTest.java
@@ -1,33 +1,35 @@
 package codingdojo;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 
 public class PlayerTest {
 
-    // choose this one if you are familiar with mocks
-    @Disabled("Test is not finished yet")
     @Test
-    void damageCalculationsWithMocks() {
-        Inventory inventory = mock(Inventory.class);
-        Stats stats = mock(Stats.class);
-        SimpleEnemy target = mock(SimpleEnemy.class);
+    void damageForProvidedExample() {
+        Equipment equipment = new Equipment(
+            new BasicItem("Round Shield", 0, 1.4f),
+            new BasicItem("Excalibur", 20, 1.5f),
+            new BasicItem("Helmet of swiftness", 0, 1.2f),
+            new BasicItem("Ten league boots", 0, 0.1f),
+            new BasicItem("Breastplate of steel", 0, 1.4f)
 
-        Damage damage = new Player(inventory, stats).calculateDamage(target);
-        assertEquals(10, damage.getAmount());
-    }
-
-    // choose this one if you are not familiar with mocks
-    @Disabled("Test is not finished yet")
-    @Test
-    void damageCalculations() {
-        Inventory inventory = new Inventory(null);
+        );
+        Inventory inventory = new Inventory(equipment);
         Stats stats = new Stats(0);
-        SimpleEnemy target = new SimpleEnemy(null, null);
-        Damage damage = new Player(inventory, stats).calculateDamage(target);
-        assertEquals(10, damage.getAmount());
+        Player player = new Player(inventory, stats);
+
+        Armor armor = new SimpleArmor(5);
+        List<Buff> buffs = List.of(
+            new BasicBuff(1.0f, 1.0f)
+        );
+        SimpleEnemy target = new SimpleEnemy(armor, buffs);
+
+        Damage damage = player.calculateDamage(target);
+
+        assertEquals(102, damage.getAmount());
     }
 }
