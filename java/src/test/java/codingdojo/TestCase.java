b41a5b775a126f3e4d930975a7bfe8d171a37713
Daniele Megna <megna.dany@gmail.com>
2023-07-25 20:14:40 +0200

    Add several test cases with base player an different enemies

--- java/src/test/java/codingdojo/TestCase.java
+++ java/src/test/java/codingdojo/TestCase.java
@@ -6,10 +6,16 @@ import static codingdojo.SimpleEnemyBuilder.anEnemy;
 public enum TestCase {
 
     PROVIDED_EXAMPLE(aPlayer(), anEnemy(), 102),
+
     ENEMY_WITH_BETTER_ARMOR(
         aPlayer(),
         anEnemy().withArmorSoak(8),
         96
+    ),
+    ENEMY_WITH_GREAT_ARMOR_CAUSE_NO_DAMAGE(
+        aPlayer(),
+        anEnemy().withArmorSoak(100),
+        0
     ),
     ENEMY_WITHOUT_BUFFS(
         aPlayer(),
@@ -19,7 +25,30 @@ public enum TestCase {
     ENEMY_WITH_SINGLE_ZERO_BUFF(
         aPlayer(),
         anEnemy().withBuffs(new BasicBuff(0, 0)),
-        107
+        107 // like without buffs
+    ),
+    ENEMY_WITH_SINGLE_BUFF_WITH_ZERO_SOAK_AND_GREAT_DAMAGE(
+        aPlayer(),
+        anEnemy().withBuffs(new BasicBuff(0, 100)),
+        107 // like without buffs
+    ),
+    ENEMY_WITH_SINGLE_BIG_BUFF(
+        aPlayer(),
+        anEnemy().withBuffs(new BasicBuff(10, 10)),
+        57
+    ),
+    ENEMY_WITH_SINGLE_BUFF_WITH_GREAT_SOAK_CAUSE_NO_DAMAGE(
+        aPlayer(),
+        anEnemy().withBuffs(new BasicBuff(100, 0)),
+        0
+    ),
+    ENEMY_WITH_MANY_BUFFS(
+        aPlayer(),
+        anEnemy().withBuffs(
+            new BasicBuff(1, 1), new BasicBuff(2.3f, 1.0f),
+            new BasicBuff(1.3f, 1.3f), new BasicBuff(1.1f, 2.1f)
+        ),
+        78
     );
 
     private final Player player;
